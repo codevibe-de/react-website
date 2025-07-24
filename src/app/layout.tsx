2d44eb44a28c60e4bc9c21051aace0033882a22a
Thomas Auinger <thomas@auinger.de>
2025-07-24 19:16:18 +0200

    applyinbg font-sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
         {children}
