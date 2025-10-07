cac73c9f3821f8d41db7ba2739607fa62f8b208b
Thomas Auinger <thomas@auinger.de>
2025-10-07 20:00:18 +0200

    changed Badge color classes

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,18 +4,18 @@ interface BadgeProps {
 }
 
 const colorClasses = {
-    red: 'bg-red-50 text-red-700 inset-ring-red-600/10 dark:bg-red-400/10 dark:text-red-400 dark:inset-ring-red-400/20',
-    blue: 'bg-blue-50 text-blue-700 inset-ring-blue-600/10 dark:bg-blue-400/10 dark:text-blue-400 dark:inset-ring-blue-400/20',
-    green: 'bg-green-50 text-green-700 inset-ring-green-600/10 dark:bg-green-400/10 dark:text-green-400 dark:inset-ring-green-400/20',
-    yellow: 'bg-yellow-50 text-yellow-700 inset-ring-yellow-600/10 dark:bg-yellow-400/10 dark:text-yellow-400 dark:inset-ring-yellow-400/20',
-    purple: 'bg-purple-50 text-purple-700 inset-ring-purple-600/10 dark:bg-purple-400/10 dark:text-purple-400 dark:inset-ring-purple-400/20',
-    gray: 'bg-gray-50 text-gray-700 inset-ring-gray-600/10 dark:bg-gray-400/10 dark:text-gray-400 dark:inset-ring-gray-400/20'
+    red: 'bg-red-100 text-red-700 inset-ring-red-600/20',
+    blue: 'bg-blue-100 text-blue-700 inset-ring-blue-600/20',
+    green: 'bg-green-100 text-green-700 inset-ring-green-600/20',
+    yellow: 'bg-yellow-100 text-yellow-700 inset-ring-yellow-600/20',
+    purple: 'bg-purple-100 text-purple-700 inset-ring-purple-600/20',
+    gray: 'bg-gray-100 text-gray-700 inset-ring-gray-600/20'
 };
 
 export default function Badge({ children, color = 'red' }: BadgeProps) {
     return (
         <span
-            className={`inline-flex items-center rounded-full px-2 py-1 text-xs font-medium inset-ring ${colorClasses[color]}`}>
+            className={`mr-1 inline-flex items-center rounded-full px-2 py-1 text-xs font-medium inset-ring ${colorClasses[color]}`}>
             {children}
         </span>
     )
